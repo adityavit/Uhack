abe319d7d5d9a62d7694af7f2c59f9be49048317
deepwiz <deepwiz@gmail.com>
2012-03-25 06:33:51 -0400

    Load Questions

--- app/manager/QuestionManager.java
+++ app/manager/QuestionManager.java
@@ -2,10 +2,13 @@ package manager;
 
 import java.util.List;
 
+import play.jobs.OnApplicationStart;
+
 import models.Question;
 import Utility.Utility;
 
 public class QuestionManager {
+		
 	public static Question getQuestion(){
 		int randomInt = Utility.getRandomNumber((int)Question.count());
 		List<Question> questions = Question.all().fetch();
