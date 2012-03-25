957e64dda4ec8cfede2384356b2f9e0c8f298139
deepwiz <deepwiz@gmail.com>
2012-03-25 02:01:51 -0400

    Working

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,12 +6,18 @@ import play.mvc.*;
 
 import java.util.*;
 
+import manager.LocationManager;
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+    
+    public static void renderVenues(String username,float latitude, float longitude){
+    	List<Location> locations = LocationManager.fetchVenues(latitude, longitude);
+    	render(locations);
     }
 
 }
