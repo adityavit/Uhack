94029f9b6c6633f9094e05e3239cec28aa0b6373
deepwiz <deepwiz@gmail.com>
2012-03-25 05:34:56 -0400

    Working

--- app/controllers/ArenaController.java
+++ app/controllers/ArenaController.java
@@ -5,11 +5,18 @@ import java.util.List;
 import play.mvc.Controller;
 
 import manager.ArenaManager;
+import manager.QuestionManager;
+import models.Question;
 import models.User;
 
 public class ArenaController extends Controller {
 	public static void renderArena(String userName){
 		List<User> usersInLocation = ArenaManager.getUsersByLocation(userName);		
 		render(usersInLocation);
+	}
+	
+	public static void getQuestion(){
+		Question question = QuestionManager.getQuestion();
+		renderJSON(question);
 	}
 }
