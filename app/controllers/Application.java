45dc38d6dd1b63c37f17fe748e3c716221ee1124
Aditya Bhatia <adityavit@gmail.com>
2012-03-25 01:02:45 -0400

    Applicaiton.java changes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,11 +5,13 @@ import play.mvc.*;
 
 import java.util.*;
 
+import manager.FoursquareManager;
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
+    	List<Location> ll = FoursquareManager.fetchVenues((float)40.72, (float)-73.99);
         render();
     }
 
